e8c9bb8d54bed21e0169f988d2d48072d002aad0
acollectionofatoms <hernandezdavid88@utexas.edu>
2015-11-17 12:25:10 -0600

    django tut: part 4 complete

--- django_tuts/mysite/polls/urls.py
+++ django_tuts/mysite/polls/urls.py
@@ -3,20 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    # ex: /polls/
-    url(r'^$',
-        views.index,
-        name='index'),
-    # ex: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$',
-        views.detail,
-        name='detail'),
-    # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$',
-        views.results,
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(),
         name='results'),
-    # ex: /polls/5/vote/
-    url(r'^(?P<question_id>[0-9]+)/vote/$',
-        views.vote,
-        name='vote'),
-    ]
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
